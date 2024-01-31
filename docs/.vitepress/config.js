2237ea07c612da1c1c5dab9a8439272b09de743b
okkgit <138770370+okkgit@users.noreply.github.com>
2024-01-31 16:49:28 +0800

    config ci

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     // 部署url blog.host.com/bar
-    // base: "/bar/",
+    base: "/myblog/",
+    outDir: '../public', // 编译结果的输出目录，配合ci文件使用
     title: "blog",
     description: "hello",
 }
